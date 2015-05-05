5de4ee3bd188026288d649a7bdf0ae9a61f02a53
ChristianConfer <confer.5.2013@gmail.com>
2015-05-05 18:16:17 -0500

    initial commit

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,25 @@
-function doClick(e) {
-    alert($.label.text);
-}
+Ti.UI.backgroundColor = 'white';
+var win = Ti.UI.createWindow();
 
-$.index.open();
+var beforeDepart = Ti.UI.createTableViewSection({ headerTitle: 'Before Departure' });
+//sectionFruit.add(Ti.UI.createTableViewRow({ title: 'Apples' }));
+//sectionFruit.add(Ti.UI.createTableViewRow({ title: 'Bananas' }));
+
+var inCountry = Ti.UI.createTableViewSection({ headerTitle: 'While In Country' });
+
+var beforeReturn = Ti.UI.createTableViewSection({ headerTitle: 'Before We Return' });
+
+//sectionVeg.add(Ti.UI.createTableViewRow({ title: 'Carrots' }));
+//sectionVeg.add(Ti.UI.createTableViewRow({ title: 'Potatoes' }));
+
+var table = Ti.UI.createTableView({
+    data: [beforeDepart, inCountry, beforeReturn]
+});
+
+table.addEventListener('click', function(e){
+    Ti.API.info(e.rowData.title);
+    Ti.API.info(e.section.headerTitle);
+});
+
+win.add(table);
+win.open();
